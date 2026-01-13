efdf0e7592ea402e31e68fb01a61d9aaf17e8fb5
Anees Ur Rehman <itxanees44@gmail.com>
2026-01-13 16:13:54 +0500

    i change

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -120,7 +120,7 @@ const experiences = [
 	},
 ];
 
-const projects :{
+const projects: {
 	name: string;
 	description: string;
 	tags: {
@@ -132,144 +132,144 @@ const projects :{
 	deploy_link: string;
 	platform: "Netlify" | "Vercel" | "Figma" | "Wordpress" | "Web"
 }[] = [
-	{
-		name: "Grillout Restaurant Website",
-		description:
-			"GrillOut is a modern restaurant website showcasing a diverse menu of burgers, steaks, wraps, and more. Designed with a sleek interface, it offers smooth navigation, optimized images, and an intuitive ordering experience to attract and engage food lovers.",
-		tags: [
-			{
-				name: "Next.js",
-				color: "red-text-gradient",
-			},
-			{
-				name: "tailwind",
-				color: "blue-text-gradient",
-			},
-		],
-		image: "/projectimg/grilloutresturantimage.png",
-		source_code_link: "https://github.com/Anees44/Grillout-Resturant-Website",
-		platform: "Vercel",
-		deploy_link: "https://grillout-resturant-website-t71v.vercel.app/",
-	},
-	{
-		name: "Scrapify – AI-Powered Web Scraper ",
-		description:
-			"Developed a smart web scraping tool that extracts data from dynamic websites, leveraging AI for content summarization and filtering.The tool offers a responsive UI with a dashboard for easy management of scraping tasks. (Under Construction)",
-		tags: [
-			{
-				name: "Next.js",
-				color: "blue-text-gradient",
-			},
-			{
-				name: "tailwind",
-				color: "green-text-gradient",
-			},
-			{
-				name: "MongoDB",
-				color: "green-text-gradient",
-			},
-		],
-		image: "/projectimg/scrapifyimage.png",
-		source_code_link: "/",
-		platform: "Vercel",
-		deploy_link: "/",
-	},
-	{
-		name: "Personal Portfolio Website",
-		description:
-			"A responsive Bank HomePage showcasing different features such as various payment gateways integration, easy money transfer, advanced security, etc. It has a beautiful interface made using Tailwind CSS and React",
-		tags: [
-			{
-				name: "Next.js",
-				color: "green-text-gradient",
-			},
-			{
-				name: "tailwind",
-				color: "blue-text-gradient",
-			},
-		],
-		image: "/projectimg/portfolioimage.png",
-		source_code_link: "https://github.com/Anees44/My-Personal-Portfolio",
-		platform: "Vercel",
-		deploy_link: "https://my-personal-portfolio-tan-three.vercel.app/",
-	},
-	{
-		name: "TextUtils Website",
-		description:
-			"TextUtils Website is a simple and user-friendly React-based tool for quick text editing tasks. It lets users convert text to uppercase or lowercase, remove extra spaces, copy content, and switch between dark and light modes. Perfect for anyone needing fast and efficient text formatting.",
-		tags: [
-			{
-				name: "react",
-				color: "blue-text-gradient",
-			},
-			{
-				name: "materialui",
-				color: "orange-text-gradient",
-			},
-			{
-				name: "MongoDB",
-				color: "green-text-gradient",
-			},
-			{
-				name: "Express",
-				color: "pink-text-gradient",
-			},
-		],
-		image: "/projectimg/Textutils.png",
-		source_code_link: "https://github.com/Anees44/TextUtils-Website",
-		platform: "Vercel",
-		deploy_link: "https://text-utils-website-wmav.vercel.app/",
-	},
-	{
-		name: "Coming Soon Project",
-		description:
-			"Stay tuned for an exciting project that blends creativity, innovation, and cutting-edge tech. Something big is on its way to redefine the digital experience.",
-		tags: [
-			{
-				name: "react",
-				color: "blue-text-gradient",
-			},
-			{
-				name: "tailwind",
-				color: "green-text-gradient",
-			},
-			{
-				name: "framer-motion",
-				color: "orange-text-gradient",
-			},
-		],
-		image: "/projectimg/comingsoonimg.png",
-		source_code_link: "/",
-		platform: "Vercel",
-		deploy_link: "/",
-	},
-	{
-		name: "Coming Soon Project",
-		description:
-			"Stay tuned for an exciting project that blends creativity, innovation, and cutting-edge tech. Something big is on its way to redefine the digital experience.",
-		tags: [
-			{
-				name: "next",
-				color: "blue-text-gradient",
-			},
-			{
-				name: "tailwind",
-				color: "green-text-gradient",
-			},
-			{
-				name: "shadCN",
-				color: "orange-text-gradient",
-			},
-			{
-				name: "mySQL",
-				color: "green-text-gradient",
-			},
-		],
-		image: "/projectimg/comingsoonimg.png",
-		source_code_link: "/",
-		platform: "Vercel",
-		deploy_link: "/",
-	},
-];
+		{
+			name: "Grillout Restaurant Website",
+			description:
+				"GrillOut is a modern restaurant website showcasing a diverse menu of burgers, steaks, wraps, and more. Designed with a sleek interface, it offers smooth navigation, optimized images, and an intuitive ordering experience to attract and engage food lovers.",
+			tags: [
+				{
+					name: "Next.js",
+					color: "red-text-gradient",
+				},
+				{
+					name: "tailwind",
+					color: "blue-text-gradient",
+				},
+			],
+			image: "/projectimg/grilloutresturantimage.png",
+			source_code_link: "https://github.com/Anees44/Grillout-Resturant-Website",
+			platform: "Vercel",
+			deploy_link: "https://grillout-resturant-website-t71v.vercel.app/",
+		},
+		{
+			name: "Scrapify – AI-Powered Web Scraper ",
+			description:
+				"Developed a smart web scraping tool that extracts data from dynamic websites, leveraging AI for content summarization and filtering.The tool offers a responsive UI with a dashboard for easy management of scraping tasks. (Under Construction)",
+			tags: [
+				{
+					name: "Next.js",
+					color: "blue-text-gradient",
+				},
+				{
+					name: "tailwnd",
+					color: "green-text-gradient",
+				},
+				{
+					name: "MongoDB",
+					color: "green-text-gradient",
+				},
+			],
+			image: "/projectimg/scrapifyimage.png",
+			source_code_link: "/",
+			platform: "Vercel",
+			deploy_link: "/",
+		},
+		{
+			name: "Personal Portfolio Website",
+			description:
+				"A responsive Bank HomePage showcasing different features such as various payment gateways integration, easy money transfer, advanced security, etc. It has a beautiful interface made using Tailwind CSS and React",
+			tags: [
+				{
+					name: "Next.js",
+					color: "green-text-gradient",
+				},
+				{
+					name: "tailwind",
+					color: "blue-text-gradient",
+				},
+			],
+			image: "/projectimg/portfolioimage.png",
+			source_code_link: "https://github.com/Anees44/My-Personal-Portfolio",
+			platform: "Vercel",
+			deploy_link: "https://my-personal-portfolio-tan-three.vercel.app/",
+		},
+		{
+			name: "TextUtils Website",
+			description:
+				"TextUtils Website is a simple and user-friendly React-based tool for quick text editing tasks. It lets users convert text to uppercase or lowercase, remove extra spaces, copy content, and switch between dark and light modes. Perfect for anyone needing fast and efficient text formatting.",
+			tags: [
+				{
+					name: "react",
+					color: "blue-text-gradient",
+				},
+				{
+					name: "materialui",
+					color: "orange-text-gradient",
+				},
+				{
+					name: "MongoDB",
+					color: "green-text-gradient",
+				},
+				{
+					name: "Express",
+					color: "pink-text-gradient",
+				},
+			],
+			image: "/projectimg/Textutils.png",
+			source_code_link: "https://github.com/Anees44/TextUtils-Website",
+			platform: "Vercel",
+			deploy_link: "https://text-utils-website-wmav.vercel.app/",
+		},
+		{
+			name: "Coming Soon Project",
+			description:
+				"Stay tuned for an exciting project that blends creativity, innovation, and cutting-edge tech. Something big is on its way to redefine the digital experience.",
+			tags: [
+				{
+					name: "react",
+					color: "blue-text-gradient",
+				},
+				{
+					name: "tailwind",
+					color: "green-text-gradient",
+				},
+				{
+					name: "framer-motion",
+					color: "orange-text-gradient",
+				},
+			],
+			image: "/projectimg/comingsoonimg.png",
+			source_code_link: "/",
+			platform: "Vercel",
+			deploy_link: "/",
+		},
+		{
+			name: "Coming Soon Project",
+			description:
+				"Stay tuned for an exciting project that blends creativity, innovation, and cutting-edge tech. Something big is on its way to redefine the digital experience.",
+			tags: [
+				{
+					name: "next",
+					color: "blue-text-gradient",
+				},
+				{
+					name: "tailwind",
+					color: "green-text-gradient",
+				},
+				{
+					name: "shadCN",
+					color: "orange-text-gradient",
+				},
+				{
+					name: "mySQL",
+					color: "green-text-gradient",
+				},
+			],
+			image: "/projectimg/comingsoonimg.png",
+			source_code_link: "/",
+			platform: "Vercel",
+			deploy_link: "/",
+		},
+	];
 
 export { services, technologies, experiences, projects };
